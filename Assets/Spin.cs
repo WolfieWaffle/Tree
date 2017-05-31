07047028e4bfff4671793cea83b5794039bb1ace
Wolfie Waffle <wolfie.waffle11@gmail.com>
2017-05-30 21:46:49 -0600

    Tree added

---
+++ Assets/Spin.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+
+public class Spin : MonoBehaviour {
+
+	public Transform t;
+
+	private const float
+	secondsToDegrees = 360f / 60f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		DateTime time = DateTime.Now;
+		t.localRotation = 
+			Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees);
+	}
+}
